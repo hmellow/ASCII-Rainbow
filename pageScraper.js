20e6350daed9faf97300bc819b1a042601f51547
PiPinecone <58414056+PiPinecone@users.noreply.github.com>
2022-07-15 12:58:10 -0600

    Completed art generator scraper

--- pageScraper.js
+++ pageScraper.js
@@ -3,6 +3,19 @@ const scraperObject = {
     async scraper(browser) {
         let page = await browser.newPage();
         console.log(`Navigating to ${this.url}`);
-        await page.goto(this.url)
+        page.goto(this.url);
+        
+        await page.waitForSelector("#inputText");
+        await page.$eval("#inputText", el => el.value = "Testing");
+
+        await page.waitForSelector("#taag_output_text")
+        const text = await page.evaluate(() => {
+            const anchor = document.querySelector("#taag_output_text");
+            return anchor.textContent;
+        });
+        console.log(text);
+        // await browser.close();
     }
 }
+
+module.exports = scraperObject;
