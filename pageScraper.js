2676af3ab1be8d87697d395776919585c8a14a2c
PiPinecone <58414056+PiPinecone@users.noreply.github.com>
2022-07-15 14:16:47 -0600

    Page reload each time

--- pageScraper.js
+++ pageScraper.js
@@ -10,31 +10,31 @@ const scraperObject = {
 
         // Open art generator page
         let page = await browser.newPage();
-        console.log(`Navigating to ${this.url}`);
-        page.goto(this.url);
-        
-
-        await page.waitForSelector("#inputText");
-        // await page.$eval("#inputText", el => el.value = "T");
+        console.log("Navigating...");
 
 
         for(let i = 0; i < inputText.length; i++) {
-            // Input each character into the text field
-            let char = String(inputText[i])
-            await page.$eval("#inputText", (el, char) => el.value = `${char}`)
+            // Open URL with text parameter
+            let newURL = this.url + encodeURIComponent(inputText[i]);
+            await page.goto(newURL);
+            await page.reload({ waitUntil: ["networkidle0", "domcontentloaded"] });
+
             // Retrieve the output text
             await page.waitForSelector("#taag_output_text");
-            const text = await page.evaluate(() => {
-                const anchor = document.querySelector("#taag_output_text");
+            let text = await page.evaluate(() => {
+                let anchor = document.querySelector("#taag_output_text");
                 return anchor.textContent;
             });
             // Add output to array
             artOutput.push(text);
         } 
 
+        
+        console.log(artOutput);
         for(let j = 0; j < artOutput.length; j++) {
             console.log(artOutput[j]);
         }
+        
         // await browser.close();
     }
 }
