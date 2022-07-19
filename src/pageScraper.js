09e26121d9a4a8a040fe598e2991facef3900425
PiPinecone <58414056+PiPinecone@users.noreply.github.com>
2022-07-18 18:09:27 -0600

    PW Rwrite: first scrape

--- src/pageScraper.js
+++ src/pageScraper.js
@@ -1,7 +1,9 @@
 const base64ToImage = require('base64-to-image');
 const fs = require('fs');
+const {chromium} = require('playwright');
 
-let inputText = "Testing";
+
+let inputText = "bcd";
 const colorsArr = ["rgb(255, 0, 0)", "rgb(255, 165, 0)", "rgb(255, 255, 0)", "rgb(0, 255, 0)", "rgb(0, 0, 255)", "rgb(143,0,255)"];
 let outputB64 = [];
 let font = "Alpha";
@@ -20,117 +22,52 @@ fs.readFile(`cache/json/${font}.json`, 'utf-8', (err, data) => {
     console.log("JSON data loaded")
 });
 
-const scraperObject = {
-    url: "http://patorjk.com/software/taag/#p=display&f=Alpha&t=",
-    async scraper(browser) {
-
-        // Open art generator page
-        let page = await browser.newPage();
-        console.log("Navigating...");
-
-        let artOutput = [];
-
-        // Split input string into individual characters
-        inputText = inputText.split("");
-        let text;
-    
-        for(let i = 0; i < inputText.length; i++) {
-            let char = inputText[i].toUpperCase();
-            
-            if (char in cache) {
-                text = cache[char];
-            } else {
-                // Open URL with text parameter
-                let newURL = this.url + encodeURIComponent(char);
-                await page.goto(newURL);
-                await page.reload({ waitUntil: ["networkidle0", "domcontentloaded"] });
-    
-                // Retrieve the output text
-                await page.waitForSelector("#taag_output_text");
-                text = await page.evaluate(() => {
-                    let anchor = document.querySelector("#taag_output_text");
-                    return anchor.textContent;
-                });
-            }
-    
-            // Add output to array
-            artOutput.push(text);
-    
-            cache[char] = text;
-        } 
-
-        // convert JSON object to string
-        const data = JSON.stringify(cache);
-
-        // write JSON string to a file
-        fs.writeFile(`./cache/json/${font}.json`, data, (err) => {
-            if (err) {
-                throw err;
-            }
-            console.log("JSON data is saved.");
-        });
+(async () => {
+    const browser = await chromium.launch({
+        headless: true
+    });
+    const page = await browser.newPage();
 
 
-        // New page
-        await page.goto("https://onlineasciitools.com/convert-ascii-to-image");
-        // Select page settings
-        await page.waitForSelector("div.page-content:nth-child(1) div.page.container:nth-child(2) div.content div.all-tools-container:nth-child(4) div.section.sides.tool.tool-primary div.body div.sides-options-wrapper > div.converter-options");
-        await page.select('[data-index="typeface"]', "monospace")
-        await page.$eval('[data-index="background-color"]', (el, color) => el.value = color, "(255, 255, 255, 0)");
-        await page.$eval('[data-index="font-size"]', el => el.value = "12px");
-        const boldCheckbox = '[data-index="bold"]';
-        await page.evaluate((boldCheckbox) => document.querySelector(boldCheckbox).click(), boldCheckbox);
+    // URL for site that generates ASCII art from text
+    const genURL = "http://patorjk.com/software/taag/#p=display&f=Alpha&t=";
+
+    let artOutput = [];
+    inputText = inputText.split("");
+    let textOut;
+
+    for(let i = 0; i < inputText.length; i++) {
+        let char = inputText[i].toUpperCase();
         
+        if (char in cache) {
+            textOut = cache[char];
+        } else {
+            // Open URL with text parameter
+            let newURL = genURL + encodeURIComponent(char);
+            await page.goto(newURL);
+            await page.reload();
 
-        // Generate base64 image URL
-        const dataUri = '[data-url="convert-image-to-data-uri"]';
-        const elements = await page.$x("//div[@class='widget widget-chain']");
-        await elements[0].click();
-        await page.waitForSelector(dataUri);
-        page.$eval(dataUri, elem => elem.click());
-
-        // Convert to image and make R A I N B O W
-        for(let i = 6; i < artOutput.length + 6; i++) {
-            // Set color
-            await page.$eval('[data-index="text-color"]', (el, color) => el.value = color, colorsArr[i % 6]);
-
-            // Input ASCII
-            await page.$eval('.data-wrapper textarea', (el, input) => el.value = input, artOutput[i - 6]);
-            await page.type('.data-wrapper textarea', " "); // To make sure input is recognized
-
-            // RM'd: b64
-
-            // Unique selector for the right copy button. Don't ask questions, just leave it alone. Please.
-            await page.waitForSelector("div.tool-chained>div:nth-child(4)>div:nth-child(1)>div:nth-child(2)>div>div:nth-child(2)>div:nth-child(1)>div:nth-child(4)");
-
-            const base64_url = await page.evaluate(_ => {
-                // Press the copy button, which selects the text
-                let copy = document.getElementsByClassName("widget-copy")[3];
-                copy.click();
-      
-                // Get and return the selected text (the base64 URL)
-                let selection = window.getSelection().toString();
-                return selection;
-            });
-
-            const path ='./cache/img/';
-
-            while (true) {
-                try {
-                    base64ToImage(base64_url, path/*, optionalObj*/);
-                    break;
-                } catch {
-                    continue;
-                }
-            }
-
-            delete base64_url;
+            // Retrieve the output text
+            textOut = await (page.locator("#taag_output_text")).textContent();
         }
 
+        // Add output to array
+        artOutput.push(textOut);
 
-        await browser.close();
+        cache[char] = textOut;
+    } 
 
-    }   
-}
+    // convert JSON object to string
+    const data = JSON.stringify(cache);
 
-module.exports = scraperObject;
+    // write JSON string to a file
+    fs.writeFile(`./cache/json/${font}.json`, data, (err) => {
+        if (err) {
+            throw err;
+        }
+        console.log("JSON data is saved.");
+    });
+
+
+    await browser.close();
+  })();
