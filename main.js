e9fa180ba88b8b3be16d27c2ffff23cf84be501b
PiPinecone <58414056+PiPinecone@users.noreply.github.com>
2022-07-18 21:40:30 -0600

    Delete puppeteer files

--- main.js
+++
@@ -1,6 +0,0 @@
-const puppeteer = require("puppeteer")
-const browserObject = require("./src/browser.js");
-const scraperController = require("./src/pageController.js");
-
-let browserInstance = browserObject.startBrowser();
-scraperController(browserInstance);
