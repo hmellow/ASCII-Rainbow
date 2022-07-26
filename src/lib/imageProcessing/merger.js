00aec8cbee7715dfcea74c2df8cc88e9601997a8
PiPinecone <58414056+PiPinecone@users.noreply.github.com>
2022-07-26 15:19:59 -0600

    Update ui

--- src/lib/imageProcessing/merger.js
+++ src/lib/imageProcessing/merger.js
@@ -1,5 +1,5 @@
 import mergeImg from "merge-img";
-import fs from "fs";
+import Jimp from "jimp";
 
 const colors = ["Red", "Orange", "Yellow", "Green", "Blue", "Violet"];
 
@@ -18,8 +18,17 @@ export function imgGen(input) {
     // Merge images
     mergeImg(outputImgs).then((img) => {
         // Save image as file
-        img.write('./out.png', () => console.log('done'));
+        img.write('./cache/img/out.png', () => console.log('done'));
     });
+
+    // Resize image
+    Jimp.read('./cache/img/out.png', (err, outImg) => {
+        if (err) throw err;
+        outImg
+          .scaleToFit(800, 200) // scale for output box
+          .write('./cache/img/out.png'); // save
+    });
+    
 
     return './out.png';
 }
